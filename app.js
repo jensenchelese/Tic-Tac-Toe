7fc301b9e43b353646573ccee85a6ab14770e3be
Chelese Jensen <jensenchelese@gmail.com>
2020-06-12 15:55:27 -0700

    Update to handle first win

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ var previousWinner;
 
 var piece = 'X';
 
+var score = {X: 0, O: 0};
+
 function togglePiece() {
   if(piece === 'X') {
     piece = 'O';
@@ -30,15 +32,42 @@ function display(id) {
     togglePiece();
 
   }
+  var arr = [];
   var element = document.getElementsByClassName("game");
-  for(var i = 0; i < element.length; i++) {
-    if (element[i].innerHTML === "") {
+  for(var j = 0; j < element.length; j++) {
+    arr.push(element[j].innerHTML)
+  }
+  for(var i = 0; i < arr.length; i++) {
+    if(arr[i] === arr[i + 3] && arr[i] === arr[i + 6] && arr[i] !== "") {
+      whoWins(arr[i])
       return;
-    } else {
-      continue;
+    }
+    if( arr[0] === arr[1] && arr[0] === arr[2] && arr[1] !== "") {
+      whoWins(arr[0])
+      return;
+    }
+    if( arr[3] === arr[4] && arr[3] === arr[5] && arr[4] !== "") {
+      console.log('should work')
+      whoWins(arr[3])
+      return
+    }
+    if( arr[6] === arr[7] && arr[7] === arr[8] && arr[7] !== "") {
+      whoWins(arr[6])
+      return;
+    }
+    if(arr[0] === arr[4] && arr[0] === arr[8] && arr[4] !== "") {
+      whoWins(arr[0])
+      return
+    }
+    if(arr[2] === arr[4] && arr[4] === arr[6] && arr[4] !== "") {
+      whoWins(arr[2])
+      return
+    }
+    if(arr.indexOf('') === -1) {
+      whoWins('tie');
+      return;
     }
   }
-  determineWinner();
 }
 
 function reset() {
@@ -54,48 +83,17 @@ function whoWins(win) {
   if (win !== 'X' && win !== 'O') {
     alert ("It's a tie!!")
   } else if (win === "X") {
+    score.X ++
     previousWinner = win;
       alert (playerX + " is the winner!!")
   } else if (win === "O") {
+    score.O ++
     previousWinner = win;
     alert (playerO + " is the winner!!")
 }
 }
 
-function determineWinner() {
-  var arr = [];
-  var winner = {};
-  var element = document.getElementsByClassName("game");
-  for(var i = 0; i < element.length; i++) {
-    arr.push(element[i].innerHTML)
-  }
-  for(var i = 0; i < arr.length; i++) {
-    if(arr[i] === arr[i + 3] && arr[i] === arr[i + 6]) {
-      winner[arr[i]] = 'yes';
-    }
-    if( arr[0] === arr[1] && arr[0] === arr[2]) {
-      winner[arr[0]] = 'yes';
-    }
-    if( arr[3] === arr[4] && arr[3] === arr[5]) {
-      console.log('should work')
-      winner[arr[3]] = 'yes';
-    }
-    if( arr[6] === arr[7] && arr[7] === arr[8]) {
-      winner[arr[6]] = 'yes';
-    }
-    if(arr[0] === arr[4] && arr[0] === arr[8]) {
-      winner[arr[0]] = 'yes';
-    }
-    if(arr[2] === arr[4] && arr[4] === arr[6]) {
-      winner[arr[4]] = 'yes';
-    }
-  }
-  if(Object.keys(winner).length !== 1) {
-    whoWins('tie');
-  } else {
-    console.log(Object.keys(winner)[0])
-    whoWins(Object.keys(winner)[0]);
-  }
+function displayScore() {
+  document.write("<h2>X Score: " + "</h2>")
 }
-
 
